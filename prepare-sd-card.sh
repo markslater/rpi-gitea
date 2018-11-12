bf1ef1baedbfc8955975001ed4a3def2266dbd7f
Mark Slater <mark.slater@mail.com>
2018-11-12 20:41:49 +0000

    Set up a service

--- prepare-sd-card.sh
+++ prepare-sd-card.sh
@@ -42,6 +42,42 @@ locales="en_GB.UTF-8"
 system_default_locale="en_GB.UTF-8"
 EOM
 
+mkdir -p "${MOUNT_POINT}/raspberrypi-ua-netinst/config/files/root/lib/systemd/system/"
+cat > "${MOUNT_POINT}/raspberrypi-ua-netinst/config/files/root/lib/systemd/system/gitea.service" <<- EOM
+[Unit]
+Description=Gitea (Git with a cup of tea)
+After=syslog.target
+After=network.target
+#After=mysqld.service
+#After=postgresql.service
+#After=memcached.service
+#After=redis.service
+
+[Service]
+# Modify these two values and uncomment them if you have
+# repos with lots of files and get an HTTP error 500 because
+# of that
+###
+#LimitMEMLOCK=infinity
+#LimitNOFILE=65535
+RestartSec=2s
+Type=simple
+User=git
+Group=git
+WorkingDirectory=/var/lib/gitea/
+ExecStart=/usr/local/bin/gitea web -c /etc/gitea/app.ini
+Restart=always
+Environment=USER=git HOME=/home/git GITEA_WORK_DIR=/var/lib/gitea
+# If you want to bind Gitea to a port below 1024 uncomment
+# the two values below
+###
+#CapabilityBoundingSet=CAP_NET_BIND_SERVICE
+#AmbientCapabilities=CAP_NET_BIND_SERVICE
+
+[Install]
+WantedBy=multi-user.target
+EOM
+
 # TODO verify GPG signature
 # TODO set up service
 # TODO reduce /etc/gitea permissions 'after configuration'
@@ -66,6 +102,10 @@ chroot /rootfs chmod 770 /etc/gitea
 
 chroot /rootfs wget --quiet --output-document /usr/local/bin/gitea https://dl.gitea.io/gitea/1.5.3/gitea-1.5.3-linux-arm-6
 chroot /rootfs chmod +x /usr/local/bin/gitea
+
+mkdir -p /etc/systemd/system/
+ln -s /lib/systemd/system/gitea.service /etc/systemd/system/gitea.service
+chroot /rootfs systemctl enable gitea
 EOM
 
 umount "${MOUNT_POINT}"
